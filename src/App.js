9362d6c424b809019f8507fbb07890288eaa78d1
gopi parasuraman <justingopijg@gmail.com>
2023-11-17 02:29:32 -0500

    Completed Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Article,CTA,Feature,Brand,Navbar } from './components';
+import { Blog,Features,Footer,Header,Possibility,WhatGpt } from './containers';
 
-function App() {
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar></Navbar>
+        <Header></Header>
+        <Brand></Brand>
+        <WhatGpt></WhatGpt>
+        <Features />
+        <Possibility></Possibility>
+        <CTA></CTA>
+        <Blog></Blog>  \
+        <Footer></Footer>
+
+      </div>
+      
+      
+       </div>
+
+  )
+    
 }
 
 export default App;
